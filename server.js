81bb75915b009e2a3f3b6eda91a5049759fa36b5
EricB <ericblancas23@gmail.com>
2019-04-27 23:37:14 -0700

    finished server

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const app = express()
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const app = express();
+// Bodyparser middleware
 app.use(
-    bodyParser.urlencoded({
-        extended: false
-    })
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
-
 app.use(bodyParser.json());
-
+// DB Config
 const db = require("./config/keys").mongoURI;
-
-mongoose.connect(db, {useNewUrlParser: true})
-    .then(()=> console.log("MongoDB connected successfully"))
-    .catch(err => console.log(err));
-
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`server up and running on ${port}!`);
-});
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
